7204ca09f89b6e1d66a719558c5053bda2cf988a
jbergmeier <jb@jbergmeier.com>
2020-07-07 21:45:38 +0200

    starting

--- src/BooksAPI.js
+++ src/BooksAPI.js
@@ -1,44 +1,49 @@
-
-const api = "https://reactnd-books-api.udacity.com"
-
+const api = 'https://reactnd-books-api.udacity.com';
 
 // Generate a unique token for storing your bookshelf data on the backend server.
-let token = localStorage.token
+let token = localStorage.token;
 if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
+  token = localStorage.token = Math.random()
+    .toString(36)
+    .substr(-8);
 
 const headers = {
-  'Accept': 'application/json',
-  'Authorization': token
-}
+  Accept: 'application/json',
+  Authorization: token,
+};
 
+// get specific book via ID (if it is in a shelf or not)
 export const get = (bookId) =>
   fetch(`${api}/books/${bookId}`, { headers })
-    .then(res => res.json())
-    .then(data => data.book)
+    .then((res) => res.json())
+    .then((data) => data.book);
 
+// Shows all books that have shelf === read, wantToRead or currentlyReading
 export const getAll = () =>
   fetch(`${api}/books`, { headers })
-    .then(res => res.json())
-    .then(data => data.books)
+    .then((res) => res.json())
+    .then((data) => data.books);
 
+// Calls to update the shelf: values should be: none, read, wantToRead or currentlyReading
 export const update = (book, shelf) =>
   fetch(`${api}/books/${book.id}`, {
     method: 'PUT',
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ shelf })
-  }).then(res => res.json())
+    body: JSON.stringify({ shelf }),
+  }).then((res) => res.json());
 
+// Search for a book, delivers all books, pick ID do what ever you want
 export const search = (query) =>
   fetch(`${api}/search`, {
     method: 'POST',
     headers: {
       ...headers,
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ query })
-  }).then(res => res.json())
-    .then(data => data.books)
+    body: JSON.stringify({ query }),
+  })
+    .then((res) => res.json())
+    .then((data) => data.books);
