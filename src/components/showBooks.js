04359402c9feaaf18b1289b7abc148f1f958e21c
Joern Bergmeier <jbergmeier@Joerns-MacBook-Pro.local>
2020-07-09 12:18:42 +0200

    fixed missing thumbnail bug

--- src/components/showBooks.js
+++ src/components/showBooks.js
@@ -24,7 +24,7 @@ class showBooks extends Component {
                         style={{
                           width: 128,
                           height: 193,
-                          backgroundImage: `url(${book.imageLinks.thumbnail})`,
+                          backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})` : '',
                         }}
                       ></div>
                       <div className='book-shelf-changer'>
